5ef44e7f597088ccd8c08dab2c2331098647fd7f
psharma0912 <poonam.sharma91212@gmail.com>
2022-11-28 23:45:43 +0000

    adding functions

--- milestone3.py
+++ milestone3.py
@@ -1,7 +1,24 @@
-while True:
-    guess=input('Enter a single letter:')
-    if guess.isalpha()==True and len(guess)==1:
-        break
-    else:
-        print('Invalid letter. Please, enter a single alphabetical character.')
+import random
 
+word_list=['apple', 'banana', 'orange', 'mango', 'grapes']
+word=random.choice(word_list)
+print(word)
+
+
+def check_guess(guess):
+    guess=guess.lower()
+
+    if guess in word:
+        print(f"Good guess! {guess} is in the word {word}.")
+    else:
+        print(f"Sorry, {guess} is not in the word. Try again.")   
+
+def ask_for_input():
+    while True:
+        guess=input('Enter a character from the user:')
+        if guess.isalpha()==True and len(guess)==1:
+            break
+        else:
+            print('Invalid letter. Please, enter a single alphabetical character.')
+    check_guess(guess)
+ask_for_input()
